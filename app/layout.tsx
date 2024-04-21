3adc0c08bf507fcfe6df5800ef6efcf7740daf09
docker-xing <150640021+docker-xing@users.noreply.github.com>
2024-04-21 17:08:12 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "ChatGPT",
+  description: "Your personal ChatGPT",
   viewport: {
     width: "device-width",
     initialScale: 1,
